e33828dca9b66c2cc9f4df4ff1cbef846aed1f2c
Wagner Sariolli <wagnersariolli@gmail.com>
2023-02-22 21:05:56 -0300

    up

--- portfolioelisa/src/App.js
+++ portfolioelisa/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Este vai ser o portfolio da Elisa DJ
+          DJ ELISA
         </p>
         <a
           className="App-link"
